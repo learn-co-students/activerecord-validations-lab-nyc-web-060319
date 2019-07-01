5e0f55274df263cdac2e8494ce10826321a190c4
eschechter <schechter.evan@gmail.com>
2019-07-01 10:33:48 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :click_bait_title
+
+  def click_bait_title
+    unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess"))
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
